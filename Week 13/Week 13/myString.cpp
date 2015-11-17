d8569616b1d72b8a14e9c4f495aeb1401c4de62f
Chris Bastian <peacethrubeats@gmail.com>
2015-11-17 12:43:41 -0800

    initial str commit

---
+++ Week 13/Week 13/myString.cpp
@@ -0,0 +1,9 @@
+//
+//  myString.cpp
+//  Week 13
+//
+//  Created by Chris Bastian on 11/17/15.
+//  Copyright (c) 2015 Chris Bastian. All rights reserved.
+//
+
+#include "myString.h"
---
+++ Week 13/Week 13/myString.h
@@ -0,0 +1,20 @@
+#ifndef myString
+#define myString
+
+#include <iostream>
+
+namespace cs_mystring
+{
+    class mystring
+    {
+        private:
+            char *string;
+        public:
+        
+    
+    
+    
+    };
+}
+
+#endif
