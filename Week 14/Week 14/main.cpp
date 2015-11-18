67ede96595b080afcb405fdd2895fdebe1163b10
Chris Bastian <peacethrubeats@gmail.com>
2015-11-18 11:17:49 -0800

    initial w14 commit

---
+++ Week 14/Week 14/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Week 14
+//
+//  Created by Chris Bastian on 11/18/15.
+//  Copyright (c) 2015 Chris Bastian. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
