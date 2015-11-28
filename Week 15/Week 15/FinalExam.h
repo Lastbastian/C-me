e242202249e452a4810a01a8746863207e11ecb5
Chris Bastian <peacethrubeats@gmail.com>
2015-11-28 13:37:50 -0800

    Final Class

--- Week 15/Week 15/FinalExam.h
+++ Week 15/Week 15/FinalExam.h
@@ -1,6 +1,50 @@
 #ifndef Week_15_FinalExam_h
 #define Week_15_FinalExam_h
+#include "GradedActivity.h"
 
+class FinalExam : public GradedActivity         // delcared derivitive of GradedActivity class
+{
+    private:
+        int numQuestions;
+        double pointsEach;
+        int numMissed;
+    
+    public:
+        FinalExam()                             // Default constructor
+        {
+            numQuestions = 0;
+            pointsEach = 0;
+            numMissed = 0;
+        }
+    
+        FinalExam(int questions, int missed)    // Constructor
+        {
+            set(questions, missed);
+        }
+    
+        void set(int, int);                      // Mutator function
+    
+        double getNumQuestions() const          // Accessor function
+        {
+            return numQuestions;
+        }
+    
+        double getPointsEach() const            // Accessor function
+        {
+            return pointsEach;
+        }
+    
+        int getNumMissed() const
+        {
+            return numMissed;
+        }
+    
+    
+    
+    
+    
+    
 
+};
 
 #endif
