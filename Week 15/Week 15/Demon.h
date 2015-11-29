ffd63d94e7513523846d09735d4a3d1acb9087d4
Chris Bastian <peacethrubeats@gmail.com>
2015-11-28 23:02:29 -0800

    test file inclusion

--- Week 15/Week 15/Demon.h
+++ Week 15/Week 15/Demon.h
@@ -5,8 +5,15 @@
 
 class demon : public creature
 {
-
+protected:
+    int type;
+    
 public:
-    getSpecies();
-    getDamage();
+    demon() : creature                                          // constructor
+    { type = 0; }
+    human(int newType, int newStrength, int newHitpoints) : creature(newStrength, newHitpoints)
+    { type = 0; }
+    
+    int getType() const
+    { return type; }
 };
