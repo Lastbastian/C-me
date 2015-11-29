26a7b86df1070789d904ac7e56bcddb22452c7dd
Chris Bastian <peacethrubeats@gmail.com>
2015-11-29 11:01:18 -0800

    udat

--- Week 15/Week 15/Creature.h
+++ Week 15/Week 15/Creature.h
@@ -5,18 +5,47 @@
 using namespace std;
 
 class creature {
-private:
+protected:
     int type;                       // 0 human, 1 cyberdemon, 2 balrog, 3 elf
     int strength;                   // how much damage this creature inflicts
     int hitpoints;                  // how much damage this creature can sustain
     string getSpecies() const;      // returns the type of the species
-public:
-    creature();             // initialize to human, 10 strength, 10 hitpoints
+public:                             // initialize to human, 10 strength, 10 hitpoints
+    creature();
     creature(int newType, int newStrength, int newHitpoints);
-    int getDamage() const;         // returns the amount of damage this Creature
-    // inflicts in one round of combat
+    int getDamage() const;          // returns the amount of damage this Creature
+                                    // inflicts in one round of combat
     
     // also include appropriate accessors and mutators
+};
+
+class human : public creature {
+    human() : creature()
+    { type = 0; }
+    human(newType) : creature(newStrength, newHitpoints)
+    { type = newType}
+
+
+
+};
+
+class elf : public creature {
+
+
+};
+
+class demon : public creature {
+
+};
+
+
+class cyberdemon : public demon {
+
+
+};
+
+class balrog : public demon {
+
 };
 
 
