0e15b21b533b941cb785bbe744d61e6a752389fe
Chris Bastian <peacethrubeats@gmail.com>
2015-12-02 22:01:01 -0800

    W16 Initial Commit

---
+++ Week 16/Week 16/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Week 16
+//
+//  Created by Chris Bastian on 12/2/15.
+//  Copyright (c) 2015 Chris Bastian. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
